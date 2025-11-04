b24d4ae8e42870b75b7a6bae9361b9acd59cb1f0
Nikolay <nmerenko07@gmail.com>
2025-11-04 17:47:17 +0000

    First Day work

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,47 @@
+//Nikolay Merenko
+//Period 6
+// 11/4/2025 - 11/8/2025
+// MyLinkedList Program
+
 public class MyLinkedList
 {
+    private static class ListNode {
+        Object value;
+        ListNode next;
+        ListNode(Object val) {
+            value = val;
+            next = null;
+    }
+
+    private ListNode head;
+    private int size;
+
+    private MyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
 
 
+    public boolean add(Object obj) {
+        ListNode newNode = new ListNode(obj); 
+        if (head == null) {
+            head = newNode;
+        } else {
+            ListNode current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+        return true;
+    }   
 }
