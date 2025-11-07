da6aa1d988a790b4fe6cf4da614a36d1b28bf25c
Nikolay <nmerenko07@gmail.com>
2025-11-07 17:56:28 +0000

    Coding done (no comments) Need to finish tester

--- MyLLTester.java
+++ MyLLTester.java
@@ -17,8 +17,30 @@ public class MyLLTester.java
   list.add("Object8");
   list.add("Object9");
   list.addFirst("Object10");
+  System.out.println();
 
-  
+  Object old = list.set(2, "Object3Modified");
+  System.out.println("Called set(2, \"Object3Modified\") -> returned old value: " + old);
+  System.out.println();
+
+
+  Object val = list.get(0);
+  System.out.println();
+
+
+  Object removedFirst = list.removeFirst();
+  System.out.println();
+
+
+  Object removedLast = list.removeLast();
+  System.out.println();
+
+  int removeIndex = 4;
+  Object removedAtIndex = list.remove(removeIndex);
+  System.out.println();
+
+
+
   } // end main
   
 } // end class
